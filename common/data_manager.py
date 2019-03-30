ed1d7d261019952031494df2fc426837dbacca56
Francois-xavier BABIN <fbabin@e3r2p6.42.fr>
2019-03-30 18:18:55 +0100

    data_manager.py

--- common/data_manager.py
+++ common/data_manager.py
@@ -1,7 +1,6 @@
 #file in setting : SESSION_DATA
 import pickle
 
-
 class DataManager():
 
     def __init__(self):
@@ -12,19 +11,22 @@ class DataManager():
 
      def load(self, pickle_file):
         data_dict = {}
-        try:
-            with open(pickle_file, "rb") as pickle_in:
-                data_dict = picke.load(pickle_in)
-        except:
-            print("Error : Could not load pickle file !")
+            try:
+                with open(pickle_file+".pickle", "rb") as pickle_in:
+                    data_dict = pickle.load(pickle_in)
+            except Exception as e:
+                print(e)
+                return (data_dict)
             return (data_dict)
-         return (data_dict)
 
      def dump(self, data_dict, pickle_file):
-        with open(pickle_file, 'wb') as pickle_out:
-            pickle.dump(data_dict, pickle_out)
+        try:
+            with open(pickle_file+".pickle", 'wb') as pickle_out:
+                pickle.dump(data_dict, pickle_out)
+        except Exception as e:
+            print(e)
 
-     def get_random_movie(self):
+#      def get_random_movie(self):
 
 #     def load_default_settings(self):
     
@@ -32,11 +34,28 @@ class DataManager():
 
 #     def get_movie(self):
 
+def pickle_load(pickle_file):
+    data_dict = {}
+    try:
+        with open(pickle_file+".pickle", "rb") as pickle_in:
+            data_dict = pickle.load(pickle_in)
+    except Exception as e:
+        print(e)
+        return (data_dict)
+    return (data_dict)
+
+def pickle_dump(data_dict, pickle_file):
+    try:
+        with open(pickle_file+".pickle", 'wb') as pickle_out:
+            pickle.dump(data_dict, pickle_out)
+    except Exception as e:
+        print(e)
 
 
 def main():
-
-pickle_out = open("dict.pickle","wb")
-pickle.dump(example_dict, pickle_out)
+    example_dict = {1:"6",2:"2",3:"f"}
+    pickle_dump(example_dict, "ex")
+    dic = pickle_load("ex")
+    print(dic)
 if __name__ == '__main__':
     main()
