79525a7e57eaac4d3c3c9b1af5ea41feb5581139
Martin <msicot@student.42.fr>
2019-03-30 18:29:32 +0100

    push to work

--- game/views.py
+++ game/views.py
@@ -1,11 +1,19 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
+from django.conf import settings
 
 def index(request):
     if request.method == 'POST':
         r = request.POST['action']
         if r: 
             if  r == 'A':
-                return render(request, 'game/map.html')
+                # Permet de rediriger lorsque il y a changement d'url
+                return redirect('worldmap')
     return render(request, 'game/index.html')
 
-# Create your views here.
+def worldmap(request):
+    print("Worldmap function")
+    content = {
+        'size': range(settings.GAME_CONFIG['size']),
+    
+    }
+    return render(request, 'game/map.html', content)
