215ef19109f35fb3b74e7aec21e4a58e766fa108
julien <jourdainjulien@gmail.com>
2019-03-30 20:02:07 +0100

    [ADD] options menu

--- game/urls.py
+++ game/urls.py
@@ -4,4 +4,8 @@ from . import views
 urlpatterns = [
         path('', views.index, name='index'),
         path('worldmap', views.worldmap, name='worldmap'),
-        ]
+        path('options', views.options, name='options'),
+        path('options/save_game', views.save_game, name='options'),
+        path('options/load_game', views.load_game, name='options'),
+        # path('moviedex', views.moviedex, name='moviedex'),
+]
--- game/views.py
+++ game/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, HttpResponse, redirect
 from django.conf import settings
 
+# Create your views here.
+
 def index(request):
     if request.method == 'POST':
         r = request.POST['action']
@@ -11,9 +13,42 @@ def index(request):
     return render(request, 'game/index.html')
 
 def worldmap(request):
-    print("Worldmap function")
     content = {
         'size': range(settings.GAME_CONFIG['size']),
-    
     }
     return render(request, 'game/map.html', content)
+
+# def moviedex(request):
+#     return (HttpResponse("moviedex"))
+
+def options(request):
+    if request.method == 'POST':
+        r = request.POST['action']
+        if r: 
+            if  r == 'A':
+                return redirect('/options/save_game')
+            elif r == 'B':
+                return redirect('worldmap')
+            elif r == 'start':
+                return redirect('/')
+    return render(request, 'game/options.html')
+
+def save_game(request):
+    if request.method == 'POST':
+        r = request.POST['action']
+        if r: 
+            if  r == 'A':
+                return (HttpResponse('DEV'))
+            elif r == 'B':
+                return redirect('/options')
+            if r == 'bas':
+                print(request.POST)
+    mooc = [
+        {'case': 'A', 'target' : True},
+        {'case': 'B', 'target' : False},
+        {'case': 'C', 'target' : False},
+    ]
+    return render(request, 'game/save_game.html', {'slots' : mooc})
+
+def load_game(request):
+    return (HttpResponse("loadgame"))
